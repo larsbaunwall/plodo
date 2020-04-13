7be11a8341e5aa5e1f64973e0efed4d919f8113e
Lars Baunwall <larslb@thinkability.dk>
2020-04-13 21:40:31 +0200

    Add regex for voting option that complies with unicode emojis

--- api/plodo.Backend.API/Validation/MultiVoteOptionValidator.cs
+++ api/plodo.Backend.API/Validation/MultiVoteOptionValidator.cs
@@ -9,7 +9,7 @@ namespace plodo.Backend.API.Validation
     {
         public override bool IsValid(object value)
         {
-            return value is IEnumerable<string> list && list.All(x => Regex.IsMatch(x, @"^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]){1}$"));
+            return value is IEnumerable<string> list && list.All(x => Regex.IsMatch(x, @"^[^\u0000-\u007F]+$"));
         }
     }
     
@@ -17,7 +17,7 @@ namespace plodo.Backend.API.Validation
     {
         public override bool IsValid(object value)
         {
-            return value is string str && Regex.IsMatch(str, @"^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]){1}$");
+            return value is string str && Regex.IsMatch(str, @"^[^\u0000-\u007F]+$");
         }
     }
 }
--- api/plodo.Backend.Services/Models/Session.cs
+++ api/plodo.Backend.Services/Models/Session.cs
@@ -15,11 +15,8 @@ namespace plodo.Backend.Services.Models
 
             public VoteOption(string name, string icon = null)
             {
-                if(!Regex.IsMatch(name, @"^[a-zA-Z-]+$"))
+                if(!Regex.IsMatch(name, @"^[^\u0000-\u007F]+$"))
                     throw new ArgumentException("name contains invalid characters", nameof(name));
-                
-                if(icon != null && !Regex.IsMatch(icon, @"^[a-zA-Z-]+$"))
-                    throw new ArgumentException("icon contains invalid characters", nameof(name));
 
                 Name = name;
                 Icon = icon;
