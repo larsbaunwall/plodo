0fe09bcbac5342afcbb21e3d2ecbc1ad359c47e0
Lars Baunwall <larslb@thinkability.dk>
2020-04-16 21:44:29 +0200

    Add code signing to windows build

---
+++ presenter-app/sign.js
@@ -0,0 +1,14 @@
+exports.default = async function(configuration) {
+    
+  const login = process.env.KV_LOGIN;
+  const password = process.env.KV_PASSWORD;
+  const url = process.env.KV_URL;
+  const keyname = process.env.KV_KEYNAME;
+
+  require("child_process").execSync(
+    `AzureSignTool.exe sign -kvu ${url} -kvi ${login} -kvs ${password} -kvc ${keyname} -tr http://timestamp.digicert.com -v ${configuration.path}`,
+    {
+      stdio: "inherit"
+    }
+  );
+};
--- presenter-app/vue.config.js
+++ presenter-app/vue.config.js
@@ -15,9 +15,16 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       customFileProtocol: "plodo://./", // Make sure to add "./" to the end of the protocol
+      copyright: "Copyright © 2020 plodo, part of thinkability ApS",
       builderOptions: {
-        publish: ["github"]
-      }
+        publish: ["github"],
+        win: {
+          appId: "think.plodo.presenter",
+          certificateSubjectName: "CN = Thinkability ApS, O = Thinkability ApS, STREET = Birk Centerpark 40, L = Herning, S = Midtjylland, PostalCode = 7400, C = DK",
+          sign: "./sign.js",
+          signDlls: true,
+        }
+      }, 
     }
   },
   configureWebpack: {
