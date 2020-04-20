f0fd072fa1a71fd6da7bd65069dcf84d0f3ab462
Lars Baunwall <larslb@thinkability.dk>
2020-04-20 11:16:19 +0200

    Add app version API for releases

---
+++ api/plodo.Backend.API/Controllers/AppController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using plodo.Backend.API.Models;
+
+namespace plodo.Backend.API.Controllers
+{
+    [Route("v{version:apiVersion}/app")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class AppController
+    {
+        /// <summary>
+        /// Get app configuration
+        /// </summary>
+        /// <param name="appVersion">The app version, defaults to latest</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{appVersion}")]
+        public async Task<ActionResult<GetAppVersionResponse>> GetVersion(string appVersion = "latest")
+        {
+            try
+            {
+                var ghRelease = appVersion == "latest"
+                    ? "https://github.com/larsbaunwall/plodo/releases/latest/download"
+                    : $"https://github.com/larsbaunwall/plodo/releases/download/{appVersion}";
+                    
+                
+                using var client = new HttpClient();
+                var winRelease =
+                    await client.GetStringAsync(
+                        $"{ghRelease}/latest.yml");
+
+                var macRelease =
+                    await client.GetStringAsync(
+                        $"{ghRelease}/latest-mac.yml");
+            
+                var yamlSerializer = new YamlDotNet.Serialization.Deserializer();
+                var win = yamlSerializer.Deserialize<Dictionary<string, object>>(winRelease);
+                var mac = yamlSerializer.Deserialize<Dictionary<string, object>>(macRelease);
+
+                return new GetAppVersionResponse
+                {
+                    Version = win["version"].ToString(),
+                    Windows = new GetAppVersionResponse.Asset
+                    {
+                        Path = $"{ghRelease}/{win["path"]}",
+                        ReleaseDate = DateTimeOffset.Parse(win["releaseDate"].ToString())
+                    },
+                    MacOS = new GetAppVersionResponse.Asset
+                    {
+                        Path = $"{ghRelease}/{mac["path"]}",
+                        ReleaseDate = DateTimeOffset.Parse(mac["releaseDate"].ToString())
+                    }
+                };
+
+            }
+            catch (HttpRequestException e)
+            {
+                return new NotFoundObjectResult($"App version '{appVersion}' not found: {e.Message}");
+            }
+        }
+    }
+}
---
+++ api/plodo.Backend.API/Models/GetAppVersionResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace plodo.Backend.API.Models
+{
+    public class GetAppVersionResponse
+    {
+        public string Version { get; set; }
+
+        public Asset Windows { get; set; }
+        public Asset MacOS { get; set; }
+        
+        public class Asset
+        {
+            public string Path { get; set; }
+            public DateTimeOffset ReleaseDate { get; set; }
+        }
+        
+    }
+}
