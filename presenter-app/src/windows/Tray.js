844f2b55387aefb1cb0ee1b38c640cc2ed14a9c7
Lars Baunwall <larslb@thinkability.dk>
2020-05-11 22:28:42 +0200

    New update dialog & logic

--- presenter-app/src/windows/Tray.js
+++ presenter-app/src/windows/Tray.js
@@ -1,22 +1,19 @@
 "use strict";
 /* global __static */
 import { app, Tray, Menu, dialog, BrowserWindow } from "electron";
+import { is, openNewGitHubIssue, debugInfo } from "electron-util";
 import path from "path";
 import mainWindow from "../windows/MainAppWindow";
-import { autoUpdater } from "electron-updater";
-import log from "electron-log";
-
-const DEBUG = process.env.NODE_ENV !== "production";
-
-autoUpdater.logger = log;
+import updater from "../common/Updater";
 
 let tray;
 
 /**
  * @returns {Tray} existing tray
  */
-const getTray = () => { return tray; };
-
+const getTray = () => {
+  return tray;
+};
 
 /**
  * @returns {Tray} new tray
@@ -34,27 +31,26 @@ const createTray = () => {
       click: () => {
         mainWindow.getWindow().webContents.openDevTools();
       },
-      visible: DEBUG
+      visible: is.development,
     },
     {
       label: "Check for updates",
-      click: async () => {
-        await showUpdateDialog(mainWindow.getWindow());
-      },
+      click: updater.checkForNewUpdates,
+      visible: !is.macAppStore && !is.windowsStore
     },
     {
       label: `plodo v${app.getVersion()}`,
-      enabled: false
+      enabled: false,
     },
     { type: "separator" },
     {
-      role: "quit" // "role": system prepared action menu
-    }, 
+      role: "quit", // "role": system prepared action menu
+    },
   ]);
 
   tray = new Tray(path.join(__static, "icon.png"));
   tray.setToolTip("plodo");
-  
+
   tray.on("right-click", () => {
     tray.popUpContextMenu(menu);
   });
@@ -64,35 +60,8 @@ const createTray = () => {
   return tray;
 };
 
-const destroy = () => {if(tray) tray.destroy();};
-
-
-/**
- * @param {BrowserWindow} mainWindow
- */
-const showUpdateDialog = async (mainWindow) => {
-  try {
-    const result = await autoUpdater.checkForUpdates();
-
-    const message = `A new version of plodo is available 🎉!\n\nVersion ${result.updateInfo.version} is available.\nYour version is ${app.getVersion()}.\n\nDo you want to update?`;
-  
-    const clicked = await dialog.showMessageBox(mainWindow, {
-      type: "info",
-      title: "Update plodo",
-      buttons: ["Yes, update when I close plodo", "Later"],
-      cancelId: 1,
-      message : message
-    });  
-
-    if(clicked === 0)
-      await autoUpdater.quitAndInstall();
-  } catch (err) {
-    await dialog.showMessageBox(mainWindow, {
-      type: "error",
-      title: "Error looking for update",
-      message : err.message
-    });
-  }
+const destroy = () => {
+  if (tray) tray.destroy();
 };
 
 export default { createTray, getTray, destroy };
